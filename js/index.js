63f63175439897135b2789ab57fe7cb723709fcb
葱花滑蛋 <conghuahuadan@163.com>
2017-09-26 08:50:38 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -1,96 +1,94 @@
 $(function () {
 
-    // ----------------------------------
+    addNavLiListener();
 
-    $('.nav>li').on('mouseenter', function (e) {
-        $(this).find('ul').css('display', 'flex');
-    })
+    recBannerCarousel();
 
-    $('.nav>li').on('mouseleave', function (e) {
-        $(this).find('ul').css('display', 'none');
-    })
+    liveTabSwitch();
 
-    // ----------------------------------
+    liveBannerCarousel();
+})
 
-    // var bannerWidth = $('.rec-carousel').width();
-    // var banners = document.querySelector('.slide');
-    // var imgIndex = 0;
-    //
-    // var ulNewLi = banners.children[0].cloneNode(true);
-    // banners.appendChild(ulNewLi);
-    //
-    // banners.style.transition = 'all .6s';
-    // banners.style.left = -imgIndex * bannerWidth + "px";
-    //
-    // var timer = setInterval(function () {
-    //     imgIndex++;
-    //     banners.style.transition = 'all .6s';
-    //     banners.style.left = -imgIndex * bannerWidth + "px";
-    // }, 3000)
-    //
-    // banners.addEventListener('webkitTransitionEnd', function () {
-    //     if (imgIndex > 4) {
-    //         imgIndex = 0;
-    //         banners.style.transition = 'all .0s';
-    //         banners.style.left = -imgIndex * bannerWidth + "px";
-    //     }
-    // })
-
-    // JQ实现
-
-    var imgWidth = $('.rec-carousel').width();
-    var banners = $('.slide');
-    var dots = $('.dot');
+function bannerCarousel($bannerCTNR) {
+    var bannerCTNR = $bannerCTNR;
+    var bannerCTNRWidth = bannerCTNR.width();
+    var banners = bannerCTNR.find('.slide');
+    var bannerArr = bannerCTNR.find('.slide > li');
+    var dotArr = bannerCTNR.find('.dot > li');
     var imgIndex = 0;
 
-    var newLi = banners.children().eq(0).clone();
-    banners.append(newLi);
+    var newBanner = bannerArr.eq(0).clone();
+    banners.append(newBanner);
 
-    var width = banners.children().length * 100 + '%'
-    banners.width(width);
+    var length = banners.children().length;
+    var bannersWidth = length * 100 + "%";
+    banners.width(bannersWidth);
 
     banners.css('transition', 'all .6s');
-    banners.css('left', -imgIndex * imgWidth);
+    banners.css('left', -imgIndex * bannerCTNRWidth);
 
     var timer;
-    clearInterval(timer);
-    timer = setInterval(autoScroll, 3000)
+    clearInterval(timer)
+    timer = setInterval(autoPlay, 5000);
 
-    function autoScroll() {
+    function autoPlay() {
         imgIndex++;
         banners.css('transition', 'all .6s');
-        banners.css('left', -imgIndex * imgWidth);
-        var dotIndex = imgIndex > 4 ? 0 : imgIndex;
-        dots.children().removeClass('active').eq(dotIndex).addClass('active');
+        banners.css('left', -imgIndex * bannerCTNRWidth);
+        var dotIndex = imgIndex >= length - 1 ? 0 : imgIndex;
+        dotArr.removeClass('active').eq(dotIndex).addClass('active');
     }
 
     banners.bind('webkitTransitionEnd', function () {
-        if (imgIndex > 4) {
+        if (imgIndex >= length - 1) {
             imgIndex = 0;
-            banners.css('transition', 'all .0s');
-            banners.css('left', -imgIndex * imgWidth);
+            banners.css('transition', 'all 0s');
+            banners.css('left', -imgIndex * bannerCTNRWidth);
         }
     })
 
-    var dotArr = dots.children();
     for (var i = 0; i < dotArr.length; i++) {
         dotArr.eq(i).attr('index', i);
         dotArr.eq(i).click(function () {
             imgIndex = $(this).attr('index');
-            console.log(imgIndex);
             clearInterval(timer);
             banners.css('transition', 'all .6s');
-            banners.css('left', -imgIndex * imgWidth);
-            dots.children().removeClass('active').eq(imgIndex).addClass('active');
-            timer = setInterval(autoScroll, 3000)
+            banners.css('left', -imgIndex * bannerCTNRWidth);
+            dotArr.removeClass('active').eq(imgIndex).addClass('active');
+            timer = setInterval(autoPlay, 5000);
         })
     }
+}
 
-    // ----------------------------------
+// 直播Banner轮播
+function liveBannerCarousel() {
+    bannerCarousel($('.live > .live-right > .tab-item > .carousel-outside > .recommend > .carousel-inside'));
+}
 
-    var tabArr = $('.tab-panel');
+// 直播Tab切换
+function liveTabSwitch() {
+    var tabArr = $('.live > .live-right > .tab > .tab-panel');
+    var tabContent = $('.tab-item > .carousel-outside');
     tabArr.mouseenter(function () {
-        console.log(this);
         tabArr.removeClass('on').eq($(this).index()).addClass('on');
+        var left = -$(this).index() * tabContent.parent().width();
+        tabContent.css('left', left);
     });
-})
+
+}
+
+// 二级导航栏添加鼠标移入移除监听
+function addNavLiListener() {
+    $('.nav-guide > .guide-wrapper > .nav > li').on('mouseenter', function (e) {
+        $(this).find('ul').css('display', 'flex');
+    })
+
+    $('.nav-guide > .guide-wrapper > .nav > li').on('mouseleave', function (e) {
+        $(this).find('ul').css('display', 'none');
+    })
+}
+
+// 推荐Banner轮播
+function recBannerCarousel() {
+    bannerCarousel($('.main-page > .recommend > .rec-carousel'));
+}
